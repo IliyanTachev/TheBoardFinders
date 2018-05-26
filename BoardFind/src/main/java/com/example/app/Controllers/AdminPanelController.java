c5bd90bd275779cfe7b93489163705d7e6e1acc6
IliyanTachev <iliqn.tachev@gmail.com>
2018-05-26 13:52:52 +0300

    added users functionality in admin panel

--- BoardFind/src/main/java/com/example/app/Controllers/AdminPanelController.java
+++ BoardFind/src/main/java/com/example/app/Controllers/AdminPanelController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminPanelController {
@@ -14,9 +15,24 @@ public class AdminPanelController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/admin/allUsers")
+//    @Autowired
+////    private UserRepository eventRepository;
+
+    @GetMapping("/admin/manage/users")
     public String getAllUsers(Model model) {
         model.addAttribute("users",this.userRepository.findAll());
-        return "admin/allUsers";
+        return "admin/users";
+    }
+
+//    @GetMapping("/admin/manage/events")
+//    public String getAllEvents(Model model) {
+//        model.addAttribute("events",this.eventRepository.findAll());
+//        return "admin/events";
+//    }
+
+    @GetMapping("/admin/manage/users/{username}")
+    public String getUser(Model model, @PathVariable String username){
+        model.addAttribute("user_data",this.userRepository.findOneByUsername(username));
+        return "admin/manageUser";
     }
 }
